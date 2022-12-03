2b109d98fd3d951739e19340de89e565494c2535
sakunisgithub <ananda2003biswas@gmial.com>
2022-12-03 22:36:39 +0530

    Initial Commit

---
+++ 00001_hello_world.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1, b = 2;
+    float pi = 3.14;
+    char c = 'a';
+    bool var = true;
+
+    cout << "Hello World!" << "\n";
+    cout << "The value of a is "<<a<<" and the value of b is "<<b<<"\n";
+    cout << "The value of pi is " <<pi<<"\n";
+    cout << "The value of c is "<<c<<"\n";
+    cout << "The value of var is "<<var<<"\n";
+
+    return 0;
+}
---
+++ 00002_sum_of_two_numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter the first number = ";
+    cin >> a;
+
+    cout << "Enter the second number = ";
+    cin >> b;
+
+    cout << "The sum of the two numbers is " << a+b;
+
+    return 0;
+}
+
+// << is called the insertion operator
+// >> is called the extraction operator
